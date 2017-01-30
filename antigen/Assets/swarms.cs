1ad6d4483fa0dd1ed5b22cbe49d70062f8b23a65
Michael Gallagher <michaelgallagher2017@u.northwestern.edu>
2017-01-29 23:55:00 -0600

    early swarm

---
+++ antigen/Assets/swarms.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swarms : MonoBehaviour {
+
+	public GameObject target;
+	public float moveSpeed = 5;
+	public float rotationSpeed =5;
+	private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		rb= GetComponent<Rigidbody2D>();
+		target = GameObject.Find("Mouse");
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.LookRotation(target.transform.position-transform.position),rotationSpeed*Time.deltaTime);
+		transform.position += transform.forward*Time.deltaTime;
+
+	}
+}
