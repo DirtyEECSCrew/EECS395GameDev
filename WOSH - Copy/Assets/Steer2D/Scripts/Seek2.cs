24c5372fc7bda60270d19c264fe3babd5bae768d
Mertzstache <ericm338@Erics-MacBook-Pro-2.local>
2017-02-09 20:14:19 -0600

    small changes

---
+++ WOSH - Copy/Assets/Steer2D/Scripts/Seek2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace Steer2D
+{
+    public class Seek2 : SteeringBehaviour
+    {
+        public Vector2 TargetPoint;
+
+        void Update()
+        {
+            //TargetPoint = Input.mousePosition; //new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            TargetPoint = Input.mousePosition;
+            TargetPoint = Camera.main.ScreenToWorldPoint(TargetPoint);
+            //transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed)
+        }
+        public override Vector2 GetVelocity()
+        {
+            return ((TargetPoint - (Vector2)transform.position).normalized * agent.MaxVelocity) - agent.CurrentVelocity;   
+        }
+    }
+}
