56f25922eb42b2853c8096f090ba1367d4d71398
gallmd95 <michaelgallagher2017@u.northwestern.edu>
2017-02-11 13:23:20 -0600

    Add files via upload

---
+++ WOSH - Copy/Assets/SwarmController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwarmController : MonoBehaviour {
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		//speed = 5f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+			
+			Debug.Log(speed);
+			transform.position = new Vector2(transform.position.x+Input.GetAxis("Horizontal")* speed * Time.deltaTime, transform.position.y+Input.GetAxis("Vertical")* speed *Time.deltaTime);
+			//gameObject.transform.position += Input.GetAxis("Horizontal")* speed * Time.deltaTime;
+			//gameObject.transform.position.y += Input.GetAxis("Vertical")* speed *Time.deltaTime;
+	}
+}
