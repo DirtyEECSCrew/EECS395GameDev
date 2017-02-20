c7d4d539200e75def0238b77da7781e3c2f6a456
Michael Gallagher <michaelgallagher2017@u.northwestern.edu>
2017-02-20 17:03:18 -0600

    Bounds

---
+++ WOSH - Copy/Assets/Bounds.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounds : MonoBehaviour {
+	public Vector2 previous;
+	public float boundRadius;
+	// Use this for initialization
+	void Start () {
+		previous = new Vector2(this.gameObject.transform.position.x, this.gameObject.transform.position.y);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if ((this.gameObject.transform.position.x*this.gameObject.transform.position.x)+(this.gameObject.transform.position.y*this.gameObject.transform.position.y) > boundRadius*boundRadius){
+			this.gameObject.transform.position = previous;
+		}
+		else{
+			previous = new Vector2(this.gameObject.transform.position.x, this.gameObject.transform.position.y);
+		}
+	}
+}
